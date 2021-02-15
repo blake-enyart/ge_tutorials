ca81571a4dd9a277e93f166f64e0382234ea728c
Blake Enyart <blake.enyart@gmail.com>
2021-02-15 00:26:16 -0700

    WIP: cutover to new schema for development and integrate in GreatExpectationsOperator

---
+++ dbt/models/staging/npi/stg_npi.sql
@@ -0,0 +1,9 @@
+SELECT  
+	 npi               AS npi
+	,entity_type_code  AS entity_type_code
+	,organization_name AS organization_name
+	,last_name         AS last_name
+	,first_name        AS first_name
+	,state             AS state_abbreviation
+	,taxonomy_code     AS taxonomy_code
+FROM {{ source('source', 'npi_small') }}
